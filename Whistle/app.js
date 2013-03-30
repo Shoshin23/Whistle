60dbcafe868720cf194f7d8761faef04bec40efe
Karthik Kannan <karthikk.314@gmail.com>
2013-03-31 01:43:03 +0530

    added code to app.js, dependencies.

--- Whistle/app.js
+++ Whistle/app.js
@@ -3,5 +3,16 @@
  * Module dependencies.
  */
 coffeeScript = require("cofee-script");
+var port, 
+    appController = require('./app/controller'),
+    express = require('express'),
+    colors = require('colors'),
+    hostname = require('hostname'),
+    path = require('path'),
+    config = require('./config/config').config,
+    app = express();
+
+start();
+
 
    
